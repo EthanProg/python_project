6e5caa34567fbd17134d1f9609e9704467ca8ff4
programcccethan@gmail.com <lwqlypsxd#123>
2016-06-23 10:00:55 +0800

    variables

--- variables.py
+++ variables.py
@@ -39,12 +39,57 @@
 # whereas tuples and lists are indexed by their numeric order, dictionaries are indexed by namesthat you
 # choose.
 
-dic = {};
-dic['lookta'] = 'oka';
-print(dic);
-print(dic['lookta']);
+# dic = {};
+# dic['lookta'] = 'oka';
+# print(dic);
+# print(dic['lookta']);
 
 # Both the keys and values methods return views, which you can assign and use like any normal view.
-print(list(dic.keys()));
-print(list(dic.values()));
+# print(list(dic.keys()));
+# print(list(dic.values()));
+
+# Python offers an interesting feature of strings. Sometimes, it is useful to be able to treat a string as though
+# it were a list of individual characters.
+# slicing
+# stringAsArray = ['lalala','ahhaha'];
+# print("%s",stringAsArray[0][0]);
+
+# Remember that, like tuples, strings are immutable. When you are slicing strings, you are actually creating
+# new strings that are copies of sections of the original string.
+
+# print(True == 1);
+# print(False == 0);
+
+# Fortunately, Python provides a shortcut that enables you to access the last element of a
+# sequence by using the number – 1, and the next - to - last  element  with  – 2, letting you reverse the order of
+# the list by using negative numbers from – 1 to the number that is the negative length of the list (– 5 in the
+# case of the last_names list).
+
+# last_names = [ "Douglass","Jefferson","Williams"];
+# print(last_names[-1]);
+# print(last_names[-2]);
+
+# slice_tuple = ('a','b','c','d','e','f');
+# print(slice_tuple[2:6]);
+# slice_list = ['a','b','c','d','e','f'];
+# print(slice_list[2:6]);
+#
+# list_new = [];
+# list_new.append(slice_list);
+# print(list_new);
+#
+# slice_list.extend(['g']);
+# print(slice_list);
+
+
+
+# To keep your lists from becoming unwieldy, a method called popenables you to remove a specific
+# reference to data from the list after you ’ re done with it. When you ’ ve removed the reference, the position
+# it occupied will be filled with whatever the next element was, and the list will be reduced by as many
+# elements as you ’ ve  popped.
+
+
+
+
+
 
