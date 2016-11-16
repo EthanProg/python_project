83ee9692f83cff0469478397b67103e434b15f08
ethan <programcccethan@gmail.com>
2016-11-16 16:29:07 +0800

    functions

--- recursion.py
+++ recursion.py
@@ -1,10 +1,13 @@
-def print_lol(the_list):
-    for each_item in the_list:
-        if isinstance(each_item, list):
-            print_lol(each_item)
-        else:
-            print(each_item)
+# def print_lol(the_list):
+#     for each_item in the_list:
+#         if isinstance(each_item, list):
+#             print_lol(each_item)
+#         else:
+#             print(each_item)
+
+import nester
 
 m_array = ['a','b','c',['d',['e','f']]]
 
-print_lol(m_array)
+# namespace
+nester.print_lol(m_array)
