29e9d730fb6766f356e544133f179ac160018457
ethan <programcccethan@gmail.com>
2016-11-17 14:40:19 +0800

    functions

--- files/pickle1.py
+++ files/pickle1.py
@@ -3,7 +3,7 @@ import pickle
 obj = {"a":1,"b":2,"c":3}
 
 # 将obj持久化保存到文件中 tmp.txt
-pickle.dump(obj, open("tmp.txt","wb"))
+pickle.dump(obj, open("tmp.txt","wb"), True)
 
 # 从 tmp.txt 中读取并恢复 obj 对象
 obj2 = pickle.load(open("tmp.txt","rb"))
