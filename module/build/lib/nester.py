fcd87efc0ececb0d4cde40c5ff0f6a6a25c50b08
ethan <programcccethan@gmail.com>
2016-11-17 08:29:00 +0800

    functions

--- module/build/lib/nester.py
+++ module/build/lib/nester.py
@@ -1,13 +1,17 @@
 """ this a comment """
 
-def print_lol(the_list):
-    for each_item in the_list:
-        if isinstance(each_item, list):
-            print_lol(each_item)
-        else:
-            print(each_item)
+# def print_lol(the_list):
+#     for each_item in the_list:
+#         if isinstance(each_item, list):
+#             print_lol(each_item)
+#         else:
+#             print(each_item)
 
-def print_lol(the_list, level):
+# 方法重载
+# 这一点要强于javascript
+# NO!!!
+# 类javascript 不支持重载 后面定义的方法会覆盖原来的
+def print_lol(the_list, level=0):
     for each_item in the_list:
         if isinstance(each_item, list):
             print_lol(each_item, level + 1)
