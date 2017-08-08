c79043175e8bcb48f6994dce0148afabe0fee90a
ethan <programcccethan@gmail.com>
2017-08-08 15:54:03 +0800

    终端数据初始化

--- class_demo.py
+++ class_demo.py
@@ -1,14 +1,20 @@
 _metaclass_ = type
 
-class MyClass:
-    @staticmethod
-    def smeth():
-        print('This is a static method')
-    @classmethod
-    def cmeth(cls):
-        print('This is a class method of',cls)
+# class MyClass:
+#     @staticmethod
+#     def smeth():
+#         print('This is a static method')
+#     @classmethod
+#     def cmeth(cls):
+#         print('This is a class method of',cls)
+#
+# print(MyClass.smeth())
+#
+# myClass = MyClass()
+# print(myClass.cmeth())
+#
+# for i in range(2,8):
+#     print(i)
 
-print(MyClass.smeth())
-
-myClass = MyClass()
-print(myClass.cmeth())
+ss = '20170124191006'
+print(ss[0:8])
---
+++ test1.py
@@ -0,0 +1,27 @@
+# 引入模块
+# -*- coding:utf-8 -*-
+
+# 常用包的函数
+import numpy as np
+import pandas as pd
+from pandas import Series,DataFrame
+from numpy.random import randn
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from scipy import stats
+import seaborn as sns
+
+# 解决显示汉字不正确问题
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+mpl.rcParams['axes.unicode_minus'] = False
+
+
+
+# dd = '210202128620.00'
+#
+# print(dd[:-3])
+
+# aa = [1,2,3]
+# print(aa.index(1))
+
+# print(range._doc_)
